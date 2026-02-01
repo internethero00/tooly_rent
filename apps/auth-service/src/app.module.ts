e268148175a9f5f5ccb92f66acbfdb60b82e0f6f
internethero00 <egormix1703@gmail.com>
2026-02-01 22:35:06 +0200

    + auth-service update

--- apps/auth-service/src/app.module.ts
+++ apps/auth-service/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './app/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { RMQModule } from 'nestjs-rmq';
+import { getRMQConfig } from './config/rmq.config';
+import { PrismaModule } from './infrastructure/prisma/prisma.module';
 
 @Module({
   imports: [
-    AuthModule
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'envs/.auth-service.env',
+    }),
+    PrismaModule,
+    RMQModule.forRootAsync(getRMQConfig()),
   ],
 })
 export class AppModule {}
--- apps/auth-service/src/app/auth/auth.module.ts
+++ apps/auth-service/src/app/auth/auth.module.ts
@@ -1,23 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from '../../presentation/controllers/auth.controller';
 import { JwtModule } from '@nestjs/jwt';
-import { PrismaModule } from '../../infrastructure/prisma/prisma.module';
 import { AuthService } from './auth.service';
-import { ConfigModule } from '@nestjs/config';
-import { RMQModule } from 'nestjs-rmq';
-import { getRMQConfig } from '../../config/rmq.config';
 import { UserRepository } from '../../infrastructure/repositories/user.repository';
 import { USER_REPOSITORY } from '../../domain/repositories/user.repository.interface';
 
 @Module({
   imports: [
     JwtModule.register({}),
-    PrismaModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: 'envs/.auth-service.env',
-    }),
-    RMQModule.forRootAsync(getRMQConfig()),
   ],
   controllers: [AuthController],
   providers: [
