eaa310591df2ecf513d3d0f874bc77d0c0180b78
internethero00 <egormix1703@gmail.com>
2026-02-05 23:09:49 +0200

    + account refresh contract

---
+++ libs/contracts/src/lib/account/account.refresh.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace AccountRefreshToken {
+  export const topic = 'account.refresh.command';
+  export class Request {
+    refresh_token !: string;
+  }
+  export class Response {
+    access_token?: string;
+    refresh_token?: string;
+  }
+}
